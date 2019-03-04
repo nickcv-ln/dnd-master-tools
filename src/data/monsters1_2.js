d1559bab80e7ae389da982c61a15e5fc6648847b
Nic Puddu <n.puddu@outlook.com>
2019-03-04 12:27:45 +0100

    added monsters page for 1/2 and 1/4

--- src/data/monsters1_2.js
+++ src/data/monsters1_2.js
@@ -1,107 +1,212 @@
 export default [
   {
     name: 'Ape',
+    page: {
+      it: 318,
+    },
   },
   {
     name: 'Black bear',
+    page: {
+      it: 330,
+    },
   },
   {
     name: 'Cockatrice',
+    page: {
+      it: 44,
+    },
   },
   {
     name: 'Crocodile',
+    page: {
+      it: 322,
+    },
   },
   {
     name: 'Darkmantle',
+    page: {
+      it: 205,
+    },
   },
   {
     name: 'Deep gnome',
+    page: {
+      it: 165,
+    },
   },
   {
     name: 'Dust mephit',
+    page: {
+      it: 218,
+    },
   },
   {
     name: 'Gas spore',
+    page: {
+      it: 139,
+    },
   },
   {
     name: 'Giant goat',
+    page: {
+      it: 320,
+    },
   },
   {
     name: 'Giant sea horse',
+    page: {
+      it: 321,
+    },
   },
   {
     name: 'Giant wasp',
+    page: {
+      it: 341,
+    },
   },
   {
     name: 'Gnoll',
+    page: {
+      it: 164,
+    },
   },
   {
     name: 'Gray ooze',
+    page: {
+      it: 215,
+    },
   },
   {
     name: 'Hobgoblin',
+    page: {
+      it: 179,
+    },
   },
   {
     name: 'Ice mephit',
+    page: {
+      it: 217,
+    },
   },
   {
     name: 'Jackalwere',
+    page: {
+      it: 269,
+    },
   },
   {
     name: 'Lizardfolk',
+    page: {
+      it: 198,
+    },
   },
   {
     name: 'Magma mephit',
+    page: {
+      it: 217,
+    },
   },
   {
     name: 'Magmin',
+    page: {
+      it: 200,
+    },
   },
   {
     name: 'Myconid adult',
+    page: {
+      it: 223,
+    },
   },
   {
     name: 'Orc',
+    page: {
+      it: 246,
+    },
   },
   {
     name: 'Piercer',
+    page: {
+      it: 47,
+    },
   },
   {
     name: 'Reef shark',
+    page: {
+      it: 339,
+    },
   },
   {
     name: 'Rust monster',
+    page: {
+      it: 261,
+    },
   },
   {
     name: 'Sahuagin',
+    page: {
+      it: 262,
+    },
   },
   {
     name: 'Satyr',
+    page: {
+      it: 266,
+    },
   },
   {
     name: 'Scout',
+    page: {
+      it: 347,
+    },
   },
   {
     name: 'Shadow',
+    page: {
+      it: 241,
+    },
   },
   {
     name: 'Swarm of insects',
+    page: {
+      it: 335,
+    },
   },
   {
     name: 'Thug',
+    page: {
+      it: 348,
+    },
   },
   {
     name: 'Tridrone',
+    page: {
+      it: 229,
+    },
   },
   {
     name: 'Vine blight',
+    page: {
+      it: 202,
+    },
   },
   {
     name: 'Warhorse',
+    page: {
+      it: 321,
+    },
   },
   {
     name: 'Warhorse skeleton',
+    page: {
+      it: 268,
+    },
   },
   {
     name: 'Worg',
+    page: {
+      it: 341,
+    },
   },
 ];
--- src/data/monsters1_4.js
+++ src/data/monsters1_4.js
@@ -1,134 +1,266 @@
 export default [
   {
     name: 'Aarakocra',
+    page: {
+      it: 12,
+    },
   },
   {
     name: 'Acolyte',
+    page: {
+      it: 342,
+    },
   },
   {
     name: 'Axe beak',
+    page: {
+      it: 319,
+    },
   },
   {
     name: 'Blink dog',
+    page: {
+      it: 320,
+    },
   },
   {
     name: 'Boar',
+    page: {
+      it: 322,
+    },
   },
   {
     name: 'Bullywug',
+    page: {
+      it: 34,
+    },
   },
   {
     name: 'Constrictor snake',
+    page: {
+      it: 337,
+    },
   },
   {
     name: 'Draft horse',
+    page: {
+      it: 321,
+    },
   },
   {
     name: 'Dretch',
+    page: {
+      it: 58,
+    },
   },
   {
     name: 'Drow',
+    page: {
+      it: 128,
+    },
   },
   {
     name: 'Duodrone',
+    page: {
+      it: 229,
+    },
   },
   {
     name: 'Elk',
+    page: {
+      it: 317,
+    },
   },
   {
     name: 'Flying Sword',
+    page: {
+      it: 238,
+    },
   },
   {
     name: 'Giant badger',
+    page: {
+      it: 339,
+    },
   },
   {
     name: 'Giant bat',
+    page: {
+      it: 331,
+    },
   },
   {
     name: 'Giant centipede',
+    page: {
+      it: 329,
+    },
   },
   {
     name: 'Giant frog',
+    page: {
+      it: 334,
+    },
   },
   {
     name: 'Giant lizard',
+    page: {
+      it: 327,
+    },
   },
   {
     name: 'Giant owl',
+    page: {
+      it: 326,
+    },
   },
   {
     name: 'Giant poisonous snake',
+    page: {
+      it: 338,
+    },
   },
   {
     name: 'Giant wolf spider',
+    page: {
+      it: 333,
+    },
   },
   {
     name: 'Goblin',
+    page: {
+      it: 167,
+    },
   },
   {
     name: 'Grimlock',
+    page: {
+      it: 176,
+    },
   },
   {
     name: 'Kenku',
+    page: {
+      it: 183,
+    },
   },
   {
     name: 'Kuo-toa',
+    page: {
+      it: 187,
+    },
   },
   {
     name: 'Mud mephit',
+    page: {
+      it: 216,
+    },
   },
   {
     name: 'Needle blight',
+    page: {
+      it: 202,
+    },
   },
   {
     name: 'Panther',
+    page: {
+      it: 330,
+    },
   },
   {
     name: 'Pixie',
+    page: {
+      it: 254,
+    },
   },
   {
     name: 'Pseudodragon',
+    page: {
+      it: 255,
+    },
   },
   {
     name: 'Pteranodon',
+    page: {
+      it: 80,
+    },
   },
   {
     name: 'Riding horse',
+    page: {
+      it: 320,
+    },
   },
   {
     name: 'Skeleton',
+    page: {
+      it: 267,
+    },
   },
   {
     name: 'Smoke mephit',
+    page: {
+      it: 216,
+    },
   },
   {
     name: 'Sprite',
+    page: {
+      it: 281,
+    },
   },
   {
     name: 'Steam mephit',
+    page: {
+      it: 218,
+    },
   },
   {
     name: 'Swarm of bats',
+    page: {
+      it: 336,
+    },
   },
   {
     name: 'Swarm of rats',
+    page: {
+      it: 337,
+    },
   },
   {
     name: 'Swarm of ravens',
+    page: {
+      it: 335,
+    },
   },
   {
     name: 'Troglodyte',
+    page: {
+      it: 289,
+    },
   },
   {
     name: 'Violet fungus',
+    page: {
+      it: 139,
+    },
   },
   {
     name: 'Winged kobold',
+    page: {
+      it: 43,
+    },
   },
   {
     name: 'Wolf',
+    page: {
+      it: 327,
+    },
   },
   {
     name: 'Zombie',
+    page: {
+      it: 316,
+    },
   },
 ];
