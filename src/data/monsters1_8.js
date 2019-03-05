fd3b6ed6682dc4cdd5f1c1801c6f1a637cfe2696
Nic Puddu <n.puddu@outlook.com>
2019-03-05 10:08:39 +0100

    monster 1/8

--- src/data/monsters1_8.js
+++ src/data/monsters1_8.js
@@ -1,68 +1,134 @@
 export default [
   {
     name: 'Blood hawk',
+    page: {
+      it: 324,
+    },
   },
   {
     name: 'Camel',
+    page: {
+      it: 319,
+    },
   },
   {
     name: 'Cultist',
+    page: {
+      it: 345,
+    },
   },
   {
     name: 'Flumph',
+    page: {
+      it: 136,
+    },
   },
   {
     name: 'Flying snake',
+    page: {
+      it: 338,
+    },
   },
   {
     name: 'Giant crab',
+    page: {
+      it: 325,
+    },
   },
   {
     name: 'Giant rat',
+    page: {
+      it: 340,
+    },
   },
   {
     name: 'Giant weasel',
+    page: {
+      it: 324,
+    },
   },
   {
     name: 'Guard',
+    page: {
+      it: 348,
+    },
   },
   {
     name: 'Kobold',
+    page: {
+      it: 43,
+    },
   },
   {
     name: 'Manes',
+    page: {
+      it: 60,
+    },
   },
   {
     name: 'Mastiff',
+    page: {
+      it: 328,
+    },
   },
   {
     name: 'Merfolk',
+    page: {
+      it: 206,
+    },
   },
   {
     name: 'Monodrone',
+    page: {
+      it: 228,
+    },
   },
   {
     name: 'Mule',
+    page: {
+      it: 329,
+    },
   },
   {
     name: 'Noble',
+    page: {
+      it: 349,
+    },
   },
   {
     name: 'Poisonous snake',
+    page: {
+      it: 338,
+    },
   },
   {
     name: 'Pony',
+    page: {
+      it: 331,
+    },
   },
   {
     name: 'Slaad tadpole',
+    page: {
+      it: 276,
+    },
   },
   {
     name: 'Stirge',
+    page: {
+      it: 291,
+    },
   },
   {
     name: 'Tribal warrior',
+    page: {
+      it: 345,
+    },
   },
   {
     name: 'Twig blight',
+    page: {
+      it: 202,
+    },
   },
 ];
