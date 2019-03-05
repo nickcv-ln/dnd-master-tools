112dd0f8bae42334188fbb03671c268570b0d17d
Nic Puddu <n.puddu@outlook.com>
2019-03-05 12:28:12 +0100

    monsters challenge 15

--- src/data/monsters15.js
+++ src/data/monsters15.js
@@ -13,17 +13,32 @@ export default [
   },
   {
     name: 'Death tyrant (in lair)',
+    page: {
+      it: 29,
+    },
   },
   {
     name: 'Mummy lord (not in lair)',
+    page: {
+      it: 233,
+    },
   },
   {
     name: 'Purple worm',
+    page: {
+      it: 299,
+    },
   },
   {
     name: 'Vampire (spellcaster)',
+    page: {
+      it: 297,
+    },
   },
   {
     name: 'Vampire (warrior)',
+    page: {
+      it: 297,
+    },
   },
 ];
