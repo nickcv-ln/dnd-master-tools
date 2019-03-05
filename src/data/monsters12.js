447ff277b2e9b8f24c9df1dd43f87556df0f6e63
Nic Puddu <n.puddu@outlook.com>
2019-03-05 12:25:05 +0100

    monsters challenge 12

--- src/data/monsters12.js
+++ src/data/monsters12.js
@@ -1,11 +1,20 @@
 export default [
   {
     name: 'Arcanaloth',
+    page: {
+      it: 313,
+    },
   },
   {
     name: 'Archmage',
+    page: {
+      it: 342,
+    },
   },
   {
     name: 'Erinyes',
+    page: {
+      it: 77,
+    },
   },
 ];
