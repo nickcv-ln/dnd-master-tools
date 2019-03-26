50f2881bff2fe50ccd9f005f3f23088614fc35f4
Nic Puddu <n.puddu@outlook.com>
2019-03-26 16:46:32 +0100

    updated to webpack 4

---
+++ docs/precache-manifest.6465cb429657f8a90fcbc47727602627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce82161c63f28c63fb18",
+    "url": "/dnd-master-tools/static/js/runtime~main.02b7c756.js"
+  },
+  {
+    "revision": "f7c6c03c738ee64c3456",
+    "url": "/dnd-master-tools/static/js/main.965ee21a.chunk.js"
+  },
+  {
+    "revision": "cde2ab285000c2529f95",
+    "url": "/dnd-master-tools/static/js/2.a165b929.chunk.js"
+  },
+  {
+    "revision": "f7c6c03c738ee64c3456",
+    "url": "/dnd-master-tools/static/css/main.8f08c8e8.chunk.css"
+  },
+  {
+    "revision": "cde2ab285000c2529f95",
+    "url": "/dnd-master-tools/static/css/2.b46b5087.chunk.css"
+  },
+  {
+    "revision": "054411b0e46d1ed74e3aa5e6e34697d7",
+    "url": "/dnd-master-tools/index.html"
+  }
+];
