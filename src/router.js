0752ffb16524bd90873514bbf26261ca58c3d011
Nic Puddu <n.puddu@outlook.com>
2019-03-27 15:55:28 +0100

    added 404 page

--- src/router.js
+++ src/router.js
@@ -4,12 +4,14 @@ import { Route, Switch } from 'react-router';
 import EncounterPageContainer from 'ui/encounters/EncounterPageContainer';
 import PartyAddPage from 'ui/parties/AddPage';
 import PartyViewPageContainer from 'ui/parties/ViewPageContainer';
+import Page404 from 'ui/common/Page404';
 
 const Router = () => (
   <Switch>
     <Route exact path="/" component={EncounterPageContainer} />
     <Route path="/parties/add" component={PartyAddPage} />
     <Route path="/parties/view/:party" component={PartyViewPageContainer} />
+    <Route component={Page404} />
   </Switch>
 );
 
